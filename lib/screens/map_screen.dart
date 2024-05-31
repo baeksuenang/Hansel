e58a73bf518bed37a820debec73c233da0eae24d
baeksuenang <baegs409@gmail.com>
2024-05-31 22:42:44 +0900

    test

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,17 +9,62 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  Location _location = Location();
 
-  final LatLng _center = const LatLng(37.7749, -122.4194);
+  final LatLng _center = const LatLng(37.7749, -122.4194); // 기본 위치 (샌프란시스코)
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _location.onLocationChanged.listen((LocationData currentLocation) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(currentLocation.latitude ?? 0.0, currentLocation.longitude ?? 0.0),
+        zoom: 15.0,
+      )));
+    });
+  }
+
+  Future<void> _requestPermission() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await _location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Google Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
     );
   }
 }
